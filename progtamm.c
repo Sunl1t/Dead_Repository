a0f4ecaccc62c8fe51eabda4621188c24f42b46f
Sunl1t <mr.serzh.smekalov@mail.ru>
2023-11-11 19:46:05 +0300

    Создал первую функцию чтения первого символа

--- progtamm.c
+++ progtamm.c
@@ -4,21 +4,18 @@ class Program
 {
     static void Main()
     {
-        char ch1 = ' '; // переменная для хранения первого символа
-        char ch2 = ' '; // переменная для хранения второго символа
-
-        while (true)
+        switch (choice)
         {
-            Console.WriteLine("Меню выбора:");
-            Console.WriteLine("1. Ввести первый символ");
-            Console.WriteLine("2. Ввести второй символ");
-            Console.WriteLine("3. Вывести каждый символ и его код ASCII");
-            Console.WriteLine("4. Вывести сумму кодов ASCII символов ch1 и ch2");
-            Console.WriteLine("0. Выйти");
-
-            Console.Write("Выберите пункт меню: ");
-            int choice = Int32.Parse(Console.ReadLine());
-
+        case 0:
+            return; // выход из программы
+        case 1:
+            Console.Write("Введите первый символ: ");
+            ch1 = Console.ReadKey().KeyChar;
+            Console.WriteLine();
+            break;
+        default:
+            Console.WriteLine("Некорректный выбор. Попробуйте снова.");
+            break;
         }
     }
 }
