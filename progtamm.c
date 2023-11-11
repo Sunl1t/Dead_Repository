d43ccf6d2353a8b2089d9a4ac25197dc3ef90ae0
Sunl1t <mr.serzh.smekalov@mail.ru>
2023-11-11 20:01:27 +0300

    Создал 4 функцию складывания кодов введённых символов

--- progtamm.c
+++ progtamm.c
@@ -7,18 +7,14 @@ class Program
         char ch1 = ' '; // переменная для хранения первого символа
         char ch2 = ' '; // переменная для хранения второго символа
 
-        while (true)
-        {
-            Console.WriteLine("Меню выбора:");
-            Console.WriteLine("1. Ввести первый символ");
-            Console.WriteLine("2. Ввести второй символ");
-            Console.WriteLine("3. Вывести каждый символ и его код ASCII");
-            Console.WriteLine("4. Вывести сумму кодов ASCII символов ch1 и ch2");
-            Console.WriteLine("0. Выйти");
-
             Console.Write("Выберите пункт меню: ");
             int choice = Int32.Parse(Console.ReadLine());
-
-        }
+            switch (choice)
+            {
+            case 4:
+                int sum = (int)ch1 + (int)ch2;
+                Console.WriteLine("Сумма кодов ASCII символов " + ch1 + " и " + ch2 + " равна " + sum);
+                break;
+            }
     }
 }
