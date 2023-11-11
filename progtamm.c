ecf5b2ac1c835c49b87b7bf892e11627ae4372c2
Sunl1t <mr.serzh.smekalov@mail.ru>
2023-11-11 19:52:53 +0300

    Создал третью функцию вывода кода символа в ASCII

--- progtamm.c
+++ progtamm.c
@@ -18,7 +18,14 @@ class Program
 
             Console.Write("Выберите пункт меню: ");
             int choice = Int32.Parse(Console.ReadLine());
+            switch (choice)
+            {
 
+            case 3:
+                Console.WriteLine("Символ " + ch1 + " имеет код ASCII " + (int)ch1);
+                Console.WriteLine("Символ " + ch2 + " имеет код ASCII " + (int)ch2);
+                break;
+            }
         }
     }
 }
